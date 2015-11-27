0842c16a719de0dd07c51e8c345fddc53ad32f47
thomeric <thomas.tournoux@u-psud.fr>
2015-11-27 08:06:34 +0100

    .\Tp2CpoB

---
+++ Tp2CpoB/src/edu/iut/tests/ApplicationLogsTest.java
@@ -0,0 +1,14 @@
+package edu.iut.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApplicationLogsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
